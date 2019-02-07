f09f4190d1cc90a4f96c5dd8ccc7a6e240a882cf
Kristóf Umann <dkszelethus@gmail.com>
2019-02-07 19:20:08 +0100

    Fixed linking error

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 cmake -G Ninja \
-  -DCMAKE_CXX_COMPILER=clang++ \
-  -DLLVM_SRC_ROOT=~/Documents/analyzer_opts/llvm \
-  -DLLVM_OBJ_ROOT=~/Documents/analyzer_opts/build/ \
-  -DLLVM_INCLUDE_DIR=~/Documents/analyzer_opts/llvm/include \
-  -DLLVM_LIB_DIR=~/Documents/analyzer_opts/build/lib \
-  -DCLANG_INCLUDE_DIR=~/Documents/analyzer_opts/clang/include \
-  -DCLANG_GENERATED_INCLUDE_DIR=~/Documents/analyzer_opts/build/tools/clang/include \
+  -DCMAKE_CXX_COMPILER=/home/eumakri/Documents/analyzer_opts/build/bin/clang++ \
+  -DLLVM_CONFIG_DIR=/home/eumakri/Documents/analyzer_opts/build/bin \
+  -DLLVM_SRC_ROOT=/home/eumakri/Documents/analyzer_opts/llvm \
+  -DLLVM_OBJ_ROOT=/home/eumakri/Documents/analyzer_opts/build/ \
+  -DLLVM_INCLUDE_DIR=/home/eumakri/Documents/analyzer_opts/llvm/include \
+  -DLLVM_LIB_DIR=/home/eumakri/Documents/analyzer_opts/build/lib \
+  -DCLANG_INCLUDE_DIR=/home/eumakri/Documents/analyzer_opts/clang/include \
+  -DCLANG_GENERATED_INCLUDE_DIR=/home/eumakri/Documents/analyzer_opts/build/tools/clang/include \
   ..
